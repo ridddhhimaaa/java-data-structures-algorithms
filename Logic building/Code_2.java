b32d57df595c68ddff255ea641fec366042958a3
Riddhima Rai <riddhimarai1704@gmail.com>
2026-07-13 04:27:10 +0530

    Initial commit

---
+++ Logic building/Code_2.java
@@ -0,0 +1,7 @@
+public class Num_2{
+    public static void main(String[] args){
+        for(int i = 10;i >=1 ;i--){
+            System.out.println(i);
+        }
+    }
+}
---
+++ Logic building/Code_4.java
@@ -0,0 +1,6 @@
+public class Num_4 {
+    public static void main(String[] args){
+        
+    }
+    
+}
